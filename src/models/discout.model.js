ba1d41615d02f7dcf47adf291bb795b8ab0fe11a
duykhanh1632003 <duykhanh1632003@gmail.com>
2024-07-29 17:24:09 +0700

    add discount

--- src/models/discout.model.js
+++ src/models/discout.model.js
@@ -5,27 +5,47 @@ const mongoose = require("mongoose"); // Erase if already required
 
 const discountSchema = new mongoose.Schema(
   {
-    discount_name: { type: String, require: true },
+    discount_name: { type: String, required: true },
     discount_description: { type: String },
-    discount_type: { type: String, default: "fixed_amount" },
-    discount_value: { type: Number, require: true },
-    discount_code: { type: String, require: true },
-    discount_start_date: { type: Date, require: true },
-    discount_end_date: { type: Date, require: true },
-    discount_max_use: { type: Number, require: true },
-    discount_uses_count: { type: Number, require: true },
-    discount_users_used: { type: Array, default: [] },
-    discount_max_uses_per_user: { type: Number, require: true },
-    discount_min_order_value: { type: Number, require: true },
-    discount_shopId: { type: mongoose.Schema.Types.ObjectId, ref: "shop" },
+    discount_type: {
+      type: String,
+      enum: ["fixed_amount", "percentage"],
+      required: true,
+    },
+    discount_value: { type: Number, required: true },
+    discount_code: { type: String, required: true },
+    discount_start_date: { type: Date, required: true },
+    discount_end_date: { type: Date, required: true },
+    discount_max_use: { type: Number, required: true },
+    discount_uses_count: { type: Number, default: 0 },
+    discount_users_used: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "user",
+      default: [],
+    },
+    discount_max_uses_per_user: { type: Number, required: true },
+    discount_min_order_value: { type: Number, required: true },
+    discount_shopId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
     discount_is_active: { type: Boolean, default: true },
-
+    discount_status: {
+      type: String,
+      enum: ["active", "draft", "scheduled", "expired"],
+      default: "draft",
+    },
     discount_applies_to: {
       type: String,
-      require: true,
+      required: true,
       enum: ["all", "specific"],
     },
-    discount_product_ids: { type: Array, default: [] },
+    discount_product_ids: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "product",
+      default: [],
+    },
   },
   {
     timestamps: true,
@@ -33,5 +53,4 @@ const discountSchema = new mongoose.Schema(
   }
 );
 
-// Tạo mô hình từ schema đã định nghĩa và xuất nó
 module.exports = { discount: mongoose.model(DOCUMENT_NAME, discountSchema) };
