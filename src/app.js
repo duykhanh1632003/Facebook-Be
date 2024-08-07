112c511f187ec5b96958a501b9eec6aef86ee4fb
duykhanh1632003 <duykhanh1632003@gmail.com>
2024-08-07 23:44:42 +0700

    add commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const compression = require("compression");
 const session = require("express-session");
 const passport = require("passport");
 const RedisStore = require("connect-redis").default;
+const config = require("./config/config"); // Import config
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize');
+const httpStatus = require("http-status")
 
 require("dotenv").config();
 require("./authGoogle");
@@ -17,15 +21,16 @@ const YAML = require("yaml");
 const fs = require("fs");
 const path = require("path");
 const swaggerUi = require('swagger-ui-express');
+const ApiError = require("./utils/ApiError");
 
 const filePath = path.resolve(__dirname, './swagger-facebook.yaml');
 const file = fs.readFileSync(filePath, 'utf8');
 const swaggerDocument = YAML.parse(file);
 
 const app = express();
-
+app.use(xss())
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:80"], // Thêm cả cổng 80 và 5173
+  origin: [config.react.url, "http://localhost:80"], // Sử dụng config.react.url thay vì giá trị cố định
   credentials: true
 }));
 app.use(cookieParser());
@@ -33,11 +38,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(compression());
+app.use(mongoSanitize());
 
 app.use(
   session({
     store: new RedisStore({ client: redisClient }),
-    secret: "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",
+    secret: config.sessionSecret, // Sử dụng giá trị từ config
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
@@ -51,4 +57,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("", require("./routes/index"));
 require("./cronJobs/updateStories");
 
-module.exports = app
+// send back a 404 error for any unknown api request
+
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+app.use()
+
+module.exports = app;
